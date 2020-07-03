9e906e44959e4a785a4e15e51c27d58cb065b183
Adriel Fernando <gamerbra39@gmail.com>
2020-07-02 21:33:09 -0300

    Add files via upload

---
+++ classroom.py
@@ -0,0 +1,55 @@
+'''
+Adriel Fernando
+
+last update at 02/07/20 | 20:51 PM
+
+'''
+
+import requests
+import webbrowser
+import clipboard
+from bs4 import BeautifulSoup
+from time import sleep
+
+header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.191"}
+
+def findLink(htmlRaw):
+	lista_brainly = []
+	bs = BeautifulSoup(htmlRaw, 'lxml')
+	for link in bs.find_all('a', href=True):
+		link = link['href']
+		if 'brainly' in link:
+			lista_brainly.append(link)
+
+	return lista_brainly
+
+def main(question):
+	url = "https://www.bing.com/search?q={}%3A+*&search=&form=QBLH".format(question.replace(" ", "+"))
+	r = requests.get(url, headers=header).text
+
+	lista = findLink(r)
+	'''
+	for link in lista:
+		webbrowser.open(link)
+	'''
+	if lista:
+		print("Links Encontrados: {}\n----\n".format(lista))
+		webbrowser.open(lista[0])
+
+print("Aplicao Rodando!")
+pesquisas = []
+
+while True:
+	sleep(0.2)
+	texto = clipboard.paste()
+	if texto:
+		print("----\nPergunta: {}".format(texto))
+		clipboard.copy("")
+	try:
+		texto = texto.split('\n')[0]
+		if(texto not in pesquisas):
+			main(texto)
+		pesquisas.append(texto)
+	except:
+		pass
+		#print("Texto sem formato!")
