73ee7a1f210a7c74c98a20a32de378e30e2654b0
Adriel Fernando <gamerbra39@gmail.com>
2021-07-19 10:48:13 -0300

    Update classroom.py

--- classroom.py
+++ classroom.py
@@ -1,15 +1,21 @@
 '''
 Adriel Fernando
 
-last update at 26/10/20 | 12:52 PM
+last update at 02/07/20 | 20:51 PM
 
 '''
 
 import requests
+from os import system
 import webbrowser
 import clipboard
 from bs4 import BeautifulSoup
 from time import sleep
+from tkinter import messagebox
+from tkinter import *
+
+root = Tk()
+root.geometry("0x0")
 
 header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.191"}
 
@@ -31,10 +37,28 @@ def main(question):
 	'''
 	for link in lista:
 		webbrowser.open(link)
+		#system("powershell -C Start-Process chrome.exe -ArgumentList @( '-incognito', '{}}' )".format(link))
 	'''
 	if lista:
 		print("Links Encontrados: {}\n----\n".format(lista))
-		webbrowser.open(lista[0])
+		num = len(lista)
+
+		if(num >= 1):
+			r = requests.get(lista[1], headers=header)
+		elif(num >= 2):
+			r = requests.get(lista[2], headers=header)
+		else:
+			r = requests.get(lista[0], headers=header)
+
+		texto = r.text
+		bs = BeautifulSoup(texto, 'lxml')
+		resposta = bs.find_all('div', {'data-test':'answer-box-text'})
+		#print(resposta)
+		for i in resposta:
+			#print(i.get_text())
+			messagebox.showinfo("Resposta!!!", "Resposta: \n{}".format(i.get_text()))
+			#print("Resposta: {}".format(i.get_text()))
+
 
 print("Aplicao Rodando!")
 pesquisas = []
@@ -45,11 +69,14 @@ while True:
 	if texto:
 		print("----\nPergunta: {}".format(texto))
 		clipboard.copy("")
-		try:
-			texto = texto.split('\n')[0]
-			if(texto not in pesquisas):
-				main(texto)
-			pesquisas.append(texto)
-		except:
-			pass
-			#print("Texto sem formato!")
+	try:
+		texto = texto.split('\n')[0]
+		if(texto not in pesquisas):
+			main(texto)
+		pesquisas.append(texto)
+	except:
+		pass
+		#print("Texto sem formato!")
+
+
+root.mainloop()
